a080c9610dde5d8d62e46c8068fcb33b38ef9238
Cooper <vs.chernii@gmail.com>
2020-08-24 19:15:23 +0300

    Create Controllers and Requests

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,6 +4,43 @@
 
 @section('content')
 <h1>Контакты</h1>
+
+@if($errors->any())
+<div class="alert alert-danger">
+  <ul>
+    @foreach($errors->all() as $error)
+      <li>{{ $error }}</li>
+    @endforeach
+  </ul>
+</div>
+@endif
+
+<form action="{{ route('contact-form') }}" method="post">
+  @csrf
+
+  <div class="form-group">
+    <label for="name">Введите имя</label>
+    <input type="text" name="name" placeholder="Введите имя" id="name" class="form-control">
+  </div>
+
+  <div class="form-group">
+    <label for="name">Email</label>
+    <input type="text" name="email" placeholder="Email" id="email" class="form-control">
+  </div>
+
+  <div class="form-group">
+    <label for="name">Тема сообщения</label>
+    <input type="text" name="subject" placeholder="Тема сообщения" id="subject" class="form-control">
+  </div>
+
+  <div class="form-group">
+    <label for="message">Сообщение</label>
+    <textarea name="message" id="message" placeholder="Введите сообщение" class="form-control"></textarea>
+  </div>
+
+  <button type="submit" class="btт btn-success">Отправить</button>
+</form>
+
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
   incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
   exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
